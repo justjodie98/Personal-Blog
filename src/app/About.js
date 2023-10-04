45044014352f5c58412572f061648a6fd2fa7370
Jodie K <jodieek98@yahoo.co.uk>
2023-10-04 20:55:28 +0100

    Add tailwind css animations

--- src/app/About.js
+++ src/app/About.js
@@ -4,17 +4,17 @@ export default function About() {
   return (
     <section>
       <ul className="font-bold">
-        <li className="text-3xl my-5">
+        <li className="text-3xl my-5 animate-wiggle animate-thrice animate-duration-1000 animate-delay-1000">
           Hi <span>there</span>,
         </li>
         <li className="text-7xl my-5 animate__animated animate__zoomInDown">
           {" "}
           I'm <span>Jodie</span>.
         </li>
-        <li className="text-2xl">A Junior Software Developer</li>
+        <li className="text-2xl animate-fade-up animate-duration-2000 animate-delay-1000">A Junior Software Developer</li>
       </ul>
-      <h1 className="font-semibold text-3xl text-fuchsia-500 my-5">About</h1>
-      <h2 className="hover:animate__animated animate__bounce">
+      <h1 className="font-semibold text-3xl text-fuchsia-500 my-5 animate-jump-in animate-delay-500">About</h1>
+      <h2 className="animate-fade  animate-duration-2000 animate-delay-1000">
         {" "}
         An Apprentice <span>JavaScript </span> Engineer Graduate with
         professional experience in writing <span>Clean</span>,{" "}
--- src/app/Skills.js
+++ src/app/Skills.js
@@ -6,12 +6,13 @@ import { SiCsswizardry } from "react-icons/si"
 export default function Skills() {
   return (
     <section>
-      <h1 className="font-semibold text-3xl text-fuchsia-500 my-5">Skills</h1>
+      <h1 className="font-semibold text-3xl text-fuchsia-500 my-5 animate-jump-in animate-delay-500">Skills</h1>
       <main className="flex justify-between ">
-        <button className="bg-none border-2 border-fuchsia-500 rounded-full py-2 px-4 cursor-text">React</button>
-        <button className="bg-none border-2 border-fuchsia-500 rounded-full py-2 px-4 cursor-text">JavaScript</button>
-        <button className="bg-none border-2 border-fuchsia-500 rounded-full py-2 px-4 cursor-text">HTML5</button>
-        <button className="bg-none border-2 border-fuchsia-500 rounded-full py-2 px-4 cursor-text">CSS</button>
+        
+        <button className="bg-none border-2 border-fuchsia-500 rounded-full py-2 px-4 cursor-text animate-fade-up animate-duration-2000 animate-delay-1000">React</button>
+        <button className="bg-none border-2 border-fuchsia-500 rounded-full py-2 px-4 cursor-text animate-fade-up animate-duration-2000 animate-delay-1000">JavaScript</button>
+        <button className="bg-none border-2 border-fuchsia-500 rounded-full py-2 px-4 cursor-text animate-fade-up animate-duration-2000 animate-delay-1000">HTML5</button>
+        <button className="bg-none border-2 border-fuchsia-500 rounded-full py-2 px-4 cursor-text animate-fade-up animate-duration-2000 animate-delay-1000">CSS</button>
         </main>
     </section>
   );
